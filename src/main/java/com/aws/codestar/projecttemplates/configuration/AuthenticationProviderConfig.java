bfaa8e8938298b98e8f605704c5f04b6255f87d8
gabalmat <gabalmat@192.168.2.6>
2018-04-21 21:30:11 +0900

    remove unused imports

--- src/main/java/com/aws/codestar/projecttemplates/configuration/AuthenticationProviderConfig.java
+++ src/main/java/com/aws/codestar/projecttemplates/configuration/AuthenticationProviderConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 
 @Configuration
 @PropertySource("classpath:application.properties")
--- src/main/java/com/aws/codestar/projecttemplates/configuration/WebSecurityConfig.java
+++ src/main/java/com/aws/codestar/projecttemplates/configuration/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
